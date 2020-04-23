4705791f3856aa5d290a6deaf6776574a61054cc
Megan Moss <megan.moss98@gmail.com>
2020-04-23 11:23:30 +1000

    Connected to Ucartify.html

--- node.js
+++ node.js
@@ -1,14 +1,22 @@
 var http = require('http');
-var date = require('./module1');
-var url = require('url');
+
 var fs = require('fs');
 
-
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'}); //status code saying all is ok
-  var q = url.parse(req.url, true).query;
-  var txt = q.year + " " + q.month;
+  fs.readFile('Ucartify.html', function(err, data){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+  
+}).listen(8080);
+
+
+//var date = require('./UCartify/module1');
+//var url = require('url');
+//res.writeHead(200, {'Content-Type': 'text/html'}); //status code saying all is ok
+  //var q = url.parse(req.url, true).query;
+  //var txt = q.year + " " + q.month;
   //res.write("The date and time are: " + date.myDateTime());
   //res.write(req.url);
-  res.end(txt);
-}).listen(8080);
+  //res.end(txt);
