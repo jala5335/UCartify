d64293a4355663a2124b126c34d1ca8c3e98780a
Megan Moss <megan.moss98@gmail.com>
2020-04-23 13:02:05 +1000

    database working

--- node.js
+++ node.js
@@ -1,7 +1,73 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var mysql = require('mysql');
 
+var con = mysql.createConnection({  
+  host: "localhost",
+  user: "root",
+  password: "12345678"
+});
+
+con.connect(function(err){
+  if (err) throw err;
+  console.log("Connected!");
+
+  //Create database
+  con.query("CREATE DATABASE IF NOT EXISTS ucartifydatabase", function (err, result){
+    if (err) throw err;
+    console.log ("Database created!");
+  });
+  con.query("USE ucartifydatabase", function (err, result){
+    if (err) throw err;
+    console.log ("Database used!");
+  });
+
+  //Create users table 
+  var users_sql = "CREATE TABLE  IF NOT EXISTS users (user_id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(45), password VARCHAR(45))";
+  con.query(users_sql, function (err, result) {
+    if (err) throw err;
+    console.log("Users table created!");
+  });
+
+  //Create products table
+  var products_sql = "CREATE TABLE IF NOT EXISTS products (product_id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(45) NOT NULL, url VARCHAR(45) NOT NULL, price DECIMAL(6) NOT NULL, user_id INT, FOREIGN KEY (user_id) REFERENCES users(user_id))";
+  con.query(products_sql, function (err, result) {
+    if (err) throw err;
+    console.log("Products table created!");
+  });
+
+  //Insert user into users table
+  //var user_sql = "INSERT INTO users (username, password) VALUES ('user1','user1')";
+  //con.query(user_sql, function (err, result) {
+  //  if (err) throw err;
+  //  console.log("User added - username: user1, password: user1!");
+  //});
+
+  //Display users 
+  var display_users_sql = "SELECT * FROM users";
+  con.query(display_users_sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+
+  //Insert product into products table
+  //var product_sql = "insert into products (name, url, price, user_id) VALUES ('shoe','url',80,1)";
+  //con.query(product_sql, function (err, result) {
+  //  if (err) throw err;
+  //  console.log("Product added");
+  //});
+
+  //Display products 
+  var display_products_sql = "SELECT * FROM products";
+  con.query(display_products_sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+
+});
+
+//Create server
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
@@ -17,13 +83,3 @@ http.createServer(function (req, res) {
   });
   
 }).listen(8080);
-
-
-//var date = require('./UCartify/module1');
-//var url = require('url');
-//res.writeHead(200, {'Content-Type': 'text/html'}); //status code saying all is ok
-  //var q = url.parse(req.url, true).query;
-  //var txt = q.year + " " + q.month;
-  //res.write("The date and time are: " + date.myDateTime());
-  //res.write(req.url);
-  //res.end(txt);
