53e2d1346052d4c7d7f65ce4f2b9ab31ef3cfe77
jala5335 <59932505+jala5335@users.noreply.github.com>
2020-04-23 16:07:56 -0600

    updated but not it chief

--- homepage.js
+++ homepage.js
@@ -1,8 +1,19 @@
 var child_id = 0;
 
-function addCard(divID, link, name, img, quant, price)
+function scanform(form, divID) {
+  var x = document.getElementById(form);
+  var text = "";
+  var i;
+  for (i = 0; i < x.length ;i++) {
+    text += x.elements[i].value + "<br>";
+  }
+  document.getElementById(divID).innerHTML = text;
+}
+
+function addCard(divID, form, link, name, img, quant, price)
 {
   var unique_id = divID+child_id;
+  var finfo = scanform(form, divID);
   var product = $('<div class="card" id = "'+unique_id+'"> <img   src="t"  height="300px" width="350px"><li class="list_group-item">Tweet</li><li class="list-group-item">Sample Tweet goes here!</li><button type="button" class="btn btn-danger" onclick="deleteCard('+unique_id+')">Remove</button>');
   //var tweet = $('<p> hello </p>');
   product.appendTo('#'+divID);
@@ -12,3 +23,4 @@ function addCard(divID, link, name, img, quant, price)
 function deleteCard(cardId){
   cardId.remove();
 }
+
