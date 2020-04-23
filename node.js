3fd68b39ec634f18fa9c6691946fec26ea7ebd44
Megan Moss <megan.moss98@gmail.com>
2020-04-23 11:51:02 +1000

    reads url for which site to load

--- node.js
+++ node.js
@@ -1,9 +1,16 @@
 var http = require('http');
-
+var url = require('url');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-  fs.readFile('Ucartify.html', function(err, data){
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+
+  fs.readFile(filename, function(err, data){
+    if (err){
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 NOT FOUND");
+    }
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
